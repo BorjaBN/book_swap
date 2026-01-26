11c5fd7c0a94fd42e8bc4433e17eb65d39fd1c0f
Borja De La Cruz Lucio <fbdelacruzl01@educarex.es>
2026-01-27 00:27:38 +0100

    Preparando el merge

--- fuente/www/book_swap/resources/views/components/button.blade.php
+++
@@ -1,9 +0,0 @@
-@if($href)
-    <a href="{{ $href }}" class="btn {{ $variant }}" {{ $attributes }}>
-        {{ $slot }}
-    </a>
-@else
-    <button type="{{ $type }}" class="btn {{ $variant }}" {{ $attributes }}>
-        {{ $slot }}
-    </button>
-@endif
--- fuente/www/book_swap/resources/views/components/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<footer>
-  <p>© 2025 BookSwap · Todos los derechos reservados</p>
-  <nav>
-    <a href="#">Aviso legal</a>
-    <a href="#">Política de privacidad</a>
-    <a href="#">Contacto</a>
-  </nav>
-</footer>
